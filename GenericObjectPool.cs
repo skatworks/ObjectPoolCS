d576e85f56c4fabd84f2a493387dc6bc334455ec
skatworks <jean_paul_gaultier@msn.com>
2020-11-30 08:33:41 +0900

    initial commit テストは未作成

---
+++ GenericObjectPool.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectPoolCS
+{
+    class GenericObjectPool : IObjectPool
+    {
+        private IPoolableObjectFactory _factory = null;
+        private LinkedList<object> _pool = null;
+
+        public GenericObjectPool(IPoolableObjectFactory pof)
+        {
+            _factory = pof;
+            _pool = new LinkedList<object>();
+        }
+
+        public object borrowObject()
+        {
+            object obj;
+            try
+            {
+                if (_pool?.Count > 0)
+                {
+                    obj = _pool.FirstOrDefault();
+                    _pool.RemoveFirst();
+                } else
+                {
+                    obj = _factory.makeObject();
+                }
+            } catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                obj = null;
+            }
+
+            return obj;
+        }
+
+        public void returnObject(object obj)
+        {
+            _pool.AddLast(obj);
+        }
+    }
+}
---
+++ GenericObjectPoolFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectPoolCS
+{
+    class GenericObjectPoolFactory : IObjectPoolFactory
+    {
+        protected IPoolableObjectFactory _factory = null;
+
+        public GenericObjectPoolFactory(IPoolableObjectFactory pof)
+        {
+            _factory = pof;
+        }
+
+        public IObjectPool createPool()
+        {
+            return new GenericObjectPool(_factory);
+        }
+    }
+}
